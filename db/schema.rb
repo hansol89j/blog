6e55cf65729c05d31e926362938466eb7ffb8284
Hansol Jeong <hansol89j@gmail.com>
2019-07-28 14:36:58 -0600

    Creating bulleint model: index form new edit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_051451) do
+ActiveRecord::Schema.define(version: 2019_07_28_044832) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
