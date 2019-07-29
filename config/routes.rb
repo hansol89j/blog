4c86f4ea07705c538b4f4a95419f5ea774ea1e8d
Hansol Jeong <hansol89j@gmail.com>
2019-07-29 01:00:14 -0600

    Finished creating post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :bulletins
+  resources :bulletins do
+    resources :posts
+  end
+
   resources :posts
+  
   root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end
