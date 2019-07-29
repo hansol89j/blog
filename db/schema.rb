ca2d2c3b02b813f81be5e3dd3a5d4d472b72a12d
Hansol Jeong <hansol89j@gmail.com>
2019-07-28 22:21:10 -0600

    Declared associations between models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_044832) do
+ActiveRecord::Schema.define(version: 2019_07_28_205404) do
 
   create_table "bulletins", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2019_07_28_044832) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "bulletin_id"
+    t.index ["bulletin_id"], name: "index_posts_on_bulletin_id"
   end
 
 end
